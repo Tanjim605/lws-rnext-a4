5ffc3bc3a5f9cd7bc9c690d7fa12c79f0a1f623e
Tanjim605 <tanjimulislam2001@gmail.com>
2025-06-10 23:33:33 +0600

    now 9 image and error and loading

--- src/components/ResultImageCard.jsx
+++ src/components/ResultImageCard.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { DownloadedContext, PromptContext } from "../context/index";
 
 import DownloadIconSvg from "./SVG/DownloadIconSvg";
@@ -7,57 +7,64 @@ export default function ResultImageCard({ src }) {
   const { prompt, width, height, model } = useContext(PromptContext);
   const { downloadedRef } = useContext(DownloadedContext);
 
-  let parameter = "",
-    downloadUrl = "";
+  const [isImageLoading, setIsImageLoading] = useState(true);
+  const [isError, setIsError] = useState(null);
 
-  // Making image link from prompts
-  if (width == 1024 && height == 1024 && model == "flux")
-    parameter = `${prompt}?&seed=${Math.floor(
-      Math.random() * 100
-    )}&model=${model}`;
-  else if (width == 1024 && height == 1024)
-    parameter = `${prompt}?&seed=${Math.floor(Math.random() * 100)}`;
-  else
-    parameter = `${prompt}?width=${width}&height=${height}&seed=${Math.floor(
-      Math.random() * 100
-    )}&model=${model}`;
+  let imageSource = useRef(""),
+    downloadUrl = useRef("");
 
-  src = encodeURI(src + parameter);
-  console.log(src);
+  useEffect(() => {
+    let parameter = "";
+    // Making image link from prompts
+    if (width == 1024 && height == 1024 && model == "flux")
+      parameter = `${prompt}?&seed=${Math.floor(
+        Math.random() * 100
+      )}&model=${model}`;
+    else if (width == 1024 && height == 1024)
+      parameter = `${prompt}?&seed=${Math.floor(Math.random() * 100)}`;
+    else
+      parameter = `${prompt}?width=${width}&height=${height}&seed=${Math.floor(
+        Math.random() * 100
+      )}&model=${model}`;
 
-  try {
-    fetch(src)
-      .then((response) => {
+    imageSource.current = encodeURI(src + parameter);
+    console.log(imageSource.current);
+
+    const fetchImage = async () => {
+      try {
+        // setIsImageLoading(true);
+        const response = await fetch(imageSource.current);
         if (!response.ok) {
           throw new Error("Failed to fetch image");
         }
-        return response.blob();
-      })
-      .then((blob) => {
+        const blob = await response.blob();
+
         const reader = new FileReader();
         reader.onload = () => {
-          downloadUrl = reader.result; // Store the base64 data URL
+          downloadUrl.current = reader.result; // Store the base64 data URL
+          setIsImageLoading(false);
         };
         reader.readAsDataURL(blob);
-      })
-      .catch((err) => {
-        console.error("Error fetching or processing the image:", err.message);
-      });
-  } catch (error) {
-    console.error("Error fetching or processing the image:", error);
-  }
+      } catch (error) {
+        console.error("Error fetching or processing the image:", error);
+        setIsError(error.message);
+        setIsImageLoading(false);
+      }
+    };
+    fetchImage();
+  }, []);
 
   function handleDownload() {
-    if (downloadUrl) {
-      const notUnique = [src, ...downloadedRef.current];
+    if (downloadUrl.current) {
+      const notUnique = [imageSource.current, ...downloadedRef.current];
       // I am using a set to keep the unique values from the array
       downloadedRef.current = [...new Set(notUnique)];
 
       // console.log(`not unique  ${notUnique} unique ${downloadedRef.current}`);
-      
+
       const link = document.createElement("a");
       link.setAttribute("download", "generatedImage.png");
-      link.href = downloadUrl;
+      link.href = downloadUrl.current;
       document.body.appendChild(link);
       link.click();
       link.remove();
@@ -66,12 +73,25 @@ export default function ResultImageCard({ src }) {
 
   return (
     <div className="image-card rounded-xl overflow-hidden cursor-pointer relative">
-      <div className="absolute bottom-2 right-2  p-1 ">
-        <button onClick={handleDownload}>
-          <DownloadIconSvg />
-        </button>
-      </div>
-      <img src={src} alt={prompt} className="w-full object-cover" />
+      {isImageLoading ? (
+        <p className="text-center align-middle  w-full h-48 bg-gray-700 font-semibold text-blue-500">Loading image...</p>
+      ) : isError ? (
+        <p className="text-center align-middle  w-full h-48 bg-gray-700 font-semibold text-red-400">Error: {isError}</p>
+      ) : (
+        <>
+          <div className="absolute bottom-2 right-2  p-1 ">
+            <button onClick={handleDownload}>
+              <DownloadIconSvg />
+            </button>
+          </div>
+          <img
+            src={imageSource.current}
+            alt={prompt}
+            className="w-full object-cover"
+          />
+        </>
+      )}
+      {/* <img src={src} alt={prompt} className="w-full object-cover" /> */}
     </div>
   );
 }
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -3,8 +3,14 @@ import ResultImageCard from "./ResultImageCard";
 export default function Results({ url }) {
   return (
     <div>
-      <h3 className="text-zinc-200 mb-4 font-bold text-lg">Result</h3>
+      <h3 className="text-zinc-200 mb-4 font-bold text-lg">Results:</h3>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
+        <ResultImageCard src={url} />
+        <ResultImageCard src={url} />
+        <ResultImageCard src={url} />
+        <ResultImageCard src={url} />
+        <ResultImageCard src={url} />
+        <ResultImageCard src={url} />
         <ResultImageCard src={url} />
         <ResultImageCard src={url} />
         <ResultImageCard src={url} />
